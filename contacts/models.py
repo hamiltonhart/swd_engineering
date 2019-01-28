46d4a7461ba87e4a519d65884472168fa14ec8b7
GEORGE HART <georgehart@GEORGEs-MacBook-Pro.local>
2019-01-28 04:56:38 -0800

    Before starting url setup. Includes temp login.html and login.css files. They will need to be modified to actually work with the Django output.

--- contacts/models.py
+++ contacts/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
 
 
 class Contact(models.Model):
+    PHONE_REGEX = RegexValidator(regex=r'^\+?1?\d{9,15}$')
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
-    # phone_number
+    phone_number = models.CharField(validators=[PHONE_REGEX], max_length=17, blank=True)
     notes = models.TextField()
 
     class Meta:
