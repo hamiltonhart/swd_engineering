023d77ec2dc3705640388746ca16dec316d36b19
George Hart <hamiltonhart@protonmail.com>
2019-03-18 11:45:00 -0700

    Update Project Detail Page with focus on the clients. Can add/update/delete from this page. For styling updated as well.

--- project_clients/forms.py
+++ project_clients/forms.py
@@ -7,6 +7,14 @@ class ProjectClientForm(forms.ModelForm):
     class Meta:
         model = ProjectClient
         fields = ["client", "client_role"]
+        # widgets = {
+        #     "client": forms.ModelChoiceField(
+        #         queryset="contacts.Contact".objects.all().order_by('first_name'), 
+        #         attrs={'placeholder': '-- Client --'}
+        #         ),
+        #     "client_role": forms.ChoiceField(attrs={'placeholder': '-- Client Role --'}),
+        # }
+
 
     def __init__(self, *args, **kwargs):
         super(ProjectClientForm, self).__init__(*args, **kwargs)
