1dc8320fbba93071db2463f4d083249cea517cd5
George Hart <hamiltonhart@protonmail.com>
2019-12-19 18:55:35 -0800

    Changed name of settings files

--- swd_engineering_project/settings-dev.py
+++ swd_engineering_project/settings-dev.py
@@ -14,13 +14,13 @@ import os
 import dj_database_url
 import json
 
-with open('/etc/django-config.json') as config_file:
-    config = json.load(config_file)
+# with open('/etc/django-config.json') as config_file:
+#     config = json.load(config_file)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-sensitive_info = "/etc/django-config.json"
+sensitive_info = os.path.join(BASE_DIR, "django-config.json")
 
 with open(sensitive_info) as config_file:
     config = json.load(config_file)
@@ -33,9 +33,9 @@ with open(sensitive_info) as config_file:
 SECRET_KEY = config["secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '45.56.94.96']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -100,25 +100,25 @@ WSGI_APPLICATION = 'swd_engineering_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-db_from_env = dj_database_url.config()
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': config['db_name'],
-    'USER': config['db_user'],
-    'PASSWORD': config['db_pass'],
-    'HOST': 'localhost',
-    'PORT': '5432',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-DATABASES['default'].update(db_from_env)
+}
+
+# db_from_env = dj_database_url.config()
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': config['db_name'],
+#     'USER': config['db_user'],
+#     'PASSWORD': config['db_pass'],
+#     'HOST': 'localhost',
+#     'PORT': '5432',
+#         }
+#     }
+# DATABASES['default'].update(db_from_env)
 
 # 'localhost'
 # '5432'
