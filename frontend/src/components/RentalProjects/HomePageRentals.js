7c9722c782501a05038543ffbe4308ef6d921912
George Hart <hamiltonhart@protonmail.com>
2020-04-15 12:08:23 -0700

    Rental Card Styling modified.

--- frontend/src/components/RentalProjects/HomePageRentals.js
+++ frontend/src/components/RentalProjects/HomePageRentals.js
@@ -3,10 +3,21 @@ import { Link } from "react-router-dom";
 import { FlexWrapper, GridWrapper } from "../../styled/containers";
 import { SectionHeading } from "../../styled/typography";
 
+import { makeStyles, Grid } from "@material-ui/core";
+
 import { RentalCard } from "./RentalCard";
 import { NewRentalModal } from "./NewRentalModal";
 
+const useStyles = makeStyles((theme) => ({
+  gridContainer: {
+    marginLeft: "20px",
+    marginRight: "20px",
+    maxWidth: "100%",
+  },
+}));
+
 export const HomePageRentals = ({ rentals }) => {
+  const classes = useStyles();
   return (
     <>
       <FlexWrapper justifyContent="space-between" padding="19px 34px">
@@ -17,11 +28,15 @@ export const HomePageRentals = ({ rentals }) => {
         <NewRentalModal homeButton={true} />
       </FlexWrapper>
 
-      <GridWrapper padding="0 0 113px 0">
-        {rentals.map(rental => (
-          <RentalCard key={rental.id} rental={rental} />
-        ))}
-      </GridWrapper>
+      <div className={classes.gridContainer}>
+        <Grid container spacing={2}>
+          {rentals.map((rental) => (
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <RentalCard key={rental.id} rental={rental} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </>
   );
 };
--- frontend/src/components/RentalProjects/RentalCard.js
+++ frontend/src/components/RentalProjects/RentalCard.js
@@ -1,50 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
+  makeStyles,
   Card,
-  FlexWrapper,
-  SimpleDiv,
-  PositionWrapper
-} from "../../styled/containers";
-import { CardHeading, CardSubheading, TextLink } from "../../styled/typography";
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
+import { FlexWrapper, SimpleDiv } from "../../styled/containers";
+import { CardSubheading } from "../../styled/typography";
 import { Dot } from "../../styled/utilities";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minWidth: "250px",
+    "&:hover": {
+      boxShadow: theme.shadows[5],
+    },
+  },
+  cardActions: {
+    display: "flex",
+    justifyContent: "flex-end",
+    backgroundColor: theme.palette.grey[200],
+  },
+}));
+
 export const RentalCard = ({ rental }) => {
-  const handleLinkClick = e => {
+  const handleLinkClick = (e) => {
     e.stopPropagation();
   };
+
+  const classes = useStyles();
   return (
     <Card
+      className={classes.root}
       as={Link}
       to={{ pathname: `/rentals/${rental.id}`, state: { rentalId: rental.id } }}
-      margin="12px 0 0 0 "
     >
-      <CardHeading>
-        {rental.season
-          ? `${rental.title} - S${rental.season}`
-          : `${rental.title}`}
-      </CardHeading>
-      <FlexWrapper justifyContent="left" padding="0">
-        <Dot color="0" />
-        <CardSubheading>{rental.abbreviation}</CardSubheading>
-      </FlexWrapper>
-      <SimpleDiv padding="8px 0 0 0 " className="handle-link">
-        <p>
-          Room: {(rental.primaryRoom && rental.primaryRoom.room.name) || "---"}
-        </p>
-        <p>Drives: {rental.totalDrives || "---"}</p>
-        <p>Config: {rental.channelConfig || "---"}</p>
-        <PositionWrapper position="absolute" bottom="15px" right="20px">
-          <TextLink
-            href={rental.filesLink}
-            target="_blank"
-            fontSize="16px"
-            onClick={e => handleLinkClick(e)}
-          >
-            GoogleDrive
-          </TextLink>
-        </PositionWrapper>
-      </SimpleDiv>
+      <CardContent>
+        <Typography
+          color="primary"
+          variant="h6"
+          component={Link}
+          to={{
+            pathname: `/rentals/${rental.id}`,
+            state: { rentalId: rental.id },
+          }}
+        >
+          {rental.season
+            ? `${rental.title} - S${rental.season}`
+            : `${rental.title}`}
+        </Typography>
+        <FlexWrapper justifyContent="left" padding="8px 0 0 0">
+          <Dot color="0" />
+          <CardSubheading>{rental.abbreviation}</CardSubheading>
+        </FlexWrapper>
+        <SimpleDiv padding="16px 0 0 0 " className="handle-link">
+          <p>
+            Room:{" "}
+            {(rental.primaryRoom && rental.primaryRoom.room.name) || "---"}
+          </p>
+          <p>Drives: {rental.totalDrives || "---"}</p>
+          <p>Config: {rental.channelConfig || "---"}</p>
+        </SimpleDiv>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          href={rental.filesLink}
+          target="_blank"
+          size="small"
+          color="primary"
+          endIcon={<OpenInNewIcon />}
+          onClick={(e) => handleLinkClick(e)}
+        >
+          Go To Files
+        </Button>
+      </CardActions>
     </Card>
   );
 };
